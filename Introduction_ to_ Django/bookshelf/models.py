c9776816330fde82b96fb2074b5a62b56f82903e
emansvt <emanmahmoudsvt@gmail.com>
2025-08-24 18:59:24 +0300

    Update models.py

--- Introduction_ to_ Django/bookshelf/models.py
+++ Introduction_ to_ Django/bookshelf/models.py
@@ -3,11 +3,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    published_date = models.DateField()
-    isbn = models.CharField(max_length=13, unique=True)
-    pages = models.IntegerField()
-    language = models.CharField(max_length=30)
+    publication_year = models.IntegerField()
 
     def __str__(self):
-        return self.title
-
+        return f"{self.title} by {self.author}"
